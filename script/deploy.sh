194920cee508627edb65a6a2b53b079a2040ceba
gitsang <sang.chen@outlook.com>
2022-04-06 14:32:53 +0800

    update ddns

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 VERSION=v0.0.10
-BINPATH=/usr/local/bin
-SERVICEPATH=/etc/systemd/system
+BIN_PATH=/usr/local/bin
+SERVICE_PATH=/etc/systemd/system
+CONF_PATH=/usr/local/etc/ddns
 
-wget https://github.com/gitsang/ddns/releases/download/${VERSION}/ddns -O ${BINPATH}/ddns
-wget https://github.com/gitsang/ddns/releases/download/${VERSION}/ddns.service -O ${SERVICEPATH}/ddns.service
+wget https://github.com/gitsang/ddns/releases/download/${VERSION}/ddns -O ${BIN_PATH}/ddns
+wget https://github.com/gitsang/ddns/releases/download/${VERSION}/ddns.service -O ${SERVICE_PATH}/ddns.service
+wget https://github.com/gitsang/ddns/releases/download/${VERSION}/ddns.yml -O ${CONF_PATH}/ddns.yml
 
 systemctl enable ddns.service
 systemctl restart ddns.service
