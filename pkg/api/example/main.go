40bf33132f43e372a64300e96795d92e20a4d7c9
sang <sang.chen@outlook.com>
2023-05-25 05:28:44 +0000

    fix: fix describe domain request

---
+++ pkg/api/example/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"ddns/pkg/api"
+	"encoding/json"
+	"fmt"
+
+	alidns20150109 "github.com/alibabacloud-go/alidns-20150109/v2/client"
+	"github.com/alibabacloud-go/tea/tea"
+)
+
+const (
+	AccessKeyId     = "*"
+	AccessKeySecret = "*"
+)
+
+func main() {
+	client, err := api.CreateClient(AccessKeyId, AccessKeySecret)
+	if err != nil {
+		panic(err)
+	}
+
+	describeDomainRecordsRequest := &alidns20150109.DescribeDomainRecordsRequest{
+		DomainName:  tea.String("home.c8g.top"),
+		RRKeyWord:   tea.String("@"),
+		TypeKeyWord: tea.String("A"),
+	}
+	resp, err := client.DescribeDomainRecords(describeDomainRecordsRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	respBytes, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("resp", string(respBytes))
+}
