db9ff30a6a1c73390a7cae796c143cf103a9a4c4
sang <sang.chen@outlook.com>
2024-11-21 16:41:53 +0800

    chore: Update configs example

--- cmd/main.go
+++ cmd/main.go
@@ -12,45 +12,45 @@ import (
 )
 
 type LogConfig struct {
-	Format    string `json:"format" default:"json" usage:"log format (json|console)"`
-	Level     string `json:"level" default:"info" usage:"log level (debug|info|warn|error)"`
-	Verbosity int    `json:"verbosity" default:"0" usage:"log verbosity (0-4)"`
+	Format    string `json:"format" yaml:"format" default:"json" usage:"log format (json|console)"`
+	Level     string `json:"level" yaml:"level" default:"info" usage:"log level (debug|info|warn|error)"`
+	Verbosity int    `json:"verbosity" yaml:"verbosity" default:"0" usage:"log verbosity (0-4)"`
 	Output    struct {
 		Stdout struct {
-			Enable bool `json:"enable" default:"true" usage:"enable stdout log"`
-		} `json:"stdout"`
+			Enable bool `json:"enable" yaml:"enable" default:"true" usage:"enable stdout log"`
+		} `json:"stdout" yaml:"stdout"`
 		Stderr struct {
-			Enable bool `json:"enable" default:"false" usage:"enable stderr log"`
-		} `json:"stderr"`
+			Enable bool `json:"enable" yaml:"enable" default:"false" usage:"enable stderr log"`
+		} `json:"stderr" yaml:"stderr"`
 		File struct {
-			Enable     bool   `json:"enable" default:"false" usage:"enable file log"`
-			Path       string `json:"path" default:"/var/log/ddns/ddns.log" usage:"log file path"`
-			MaxSize    string `json:"maxSize" default:"10mb" usage:"log file max size using SI(decimal) standard (K|mb|Gb...)"`
-			MaxAge     string `json:"maxAge" default:"7d" usage:"log file max age (d|h|m|s)"`
-			MaxBackups int    `json:"maxBackups" default:"3" usage:"log file max backups"`
-			Compress   bool   `json:"compress" default:"true" usage:"enable log file compress"`
-		} `json:"file"`
-	} `json:"output"`
+			Enable     bool   `json:"enable" yaml:"enable" default:"false" usage:"enable file log"`
+			Path       string `json:"path" yaml:"path" default:"/var/log/ddns/ddns.log" usage:"log file path"`
+			MaxSize    string `json:"maxSize" yaml:"maxSize" default:"10mb" usage:"log file max size using SI(decimal) standard (K|mb|Gb...)"`
+			MaxAge     string `json:"maxAge" yaml:"maxAge" default:"7d" usage:"log file max age (d|h|m|s)"`
+			MaxBackups int    `json:"maxBackups" yaml:"maxBackups" default:"10" usage:"log file max backups"`
+			Compress   bool   `json:"compress" yaml:"compress" default:"true" usage:"enable log file compress"`
+		} `json:"file" yaml:"file"`
+	} `json:"output" yaml:"output"`
 }
 
 type DdnsConfig struct {
-	Enable    bool   `json:"enable" default:"false"`
-	Type      string `json:"type" default:"A"`
-	RR        string `json:"rr" default:"example.com"`
-	Interface string `json:"interface" default:"eth0"`
-	Prefix    string `json:"prefix" default:"192.168"`
+	Enable    bool   `json:"enable" yaml:"enable" default:"false"`
+	Type      string `json:"type" yaml:"type" default:"A"`
+	RR        string `json:"rr" yaml:"rr" default:"example.com"`
+	Interface string `json:"interface" yaml:"interface" default:"eth0"`
+	Prefix    string `json:"prefix" yaml:"prefix" default:"192.168"`
 }
 
 type Config struct {
 	Log struct {
-		Default LogConfig   `json:"default"`
-		Fanouts []LogConfig `json:"fanouts"`
-	} `json:"log"`
-	AccessKeyId     string       `json:"accessKeyId" default:"changit" usage:"aliyun access key id"`
-	AccessKeySecret string       `json:"accessKeySecret" default:"changeit" usage:"aliyun access key secret"`
-	Domain          string       `json:"domain" default:"example.com" usage:"your domain"`
-	UpdateInterval  string       `json:"updateInterval" default:"1h" usage:"the interval to check and update dns record in duration format"`
-	Ddnss           []DdnsConfig `json:"ddnss"`
+		Default LogConfig   `json:"default" yaml:"default"`
+		Fanouts []LogConfig `json:"fanouts" yaml:"fanouts"`
+	} `json:"log" yaml:"log"`
+	AccessKeyId     string       `json:"accessKeyId" yaml:"accessKeyId" default:"changit" usage:"aliyun access key id"`
+	AccessKeySecret string       `json:"accessKeySecret" yaml:"accessKeySecret" default:"changeit" usage:"aliyun access key secret"`
+	Domain          string       `json:"domain" yaml:"domain" default:"example.com" usage:"your domain"`
+	UpdateInterval  string       `json:"updateInterval" yaml:"updateInterval" default:"1h" usage:"the interval to check and update dns record in duration format"`
+	Ddnss           []DdnsConfig `json:"ddnss" yaml:"ddnss"`
 }
 
 var rootCmd = &cobra.Command{
